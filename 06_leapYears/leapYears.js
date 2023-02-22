e4e98675bab43c049fc60506911b7290a45fef78
perugi <dominik.perusko@gmail.com>
2023-02-22 20:55:31 +0100

    Add solution for exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+    if (!(year % 4) && !(year % 400)) return true;
+    if (!(year % 4) && !(year % 100)) return false;
+    if (!(year % 4)) return true;
+    return false
 };
 
 // Do not edit below this line
