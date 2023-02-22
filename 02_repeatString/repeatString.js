2e953cffd8430747a64e68b6985fe6a0707126ad
perugi <dominik.perusko@gmail.com>
2023-02-22 20:16:29 +0100

    Add solution for exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function (inString, repeat) {
+    if (repeat < 0) return "ERROR";
+    let outString = "";
 
+    for (let i = 0; i < repeat; i++) {
+        outString += inString;
+    }
+
+    return outString;
 };
 
 // Do not edit below this line
