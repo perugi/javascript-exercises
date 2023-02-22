434a357d7c6059aac8cf4daccbb609b0f832bc52
perugi <dominik.perusko@gmail.com>
2023-02-22 20:50:53 +0100

    Add solution for exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function (start, stop) {
+    if (typeof start !== "number" || typeof stop !== "number" || start < 0 || stop < 0) return "ERROR";
+
+    let lower, upper;
+    if (start <= stop) {
+        lower = start;
+        upper = stop;
+    } else {
+        lower = stop;
+        upper = start;
+    }
+    let sum = 0;
+    for (let i = lower; i <= upper; i++) {
+        sum += i;
+    }
+
+    return sum
 
 };
 
