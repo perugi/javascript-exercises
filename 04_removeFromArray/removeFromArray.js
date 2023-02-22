b8d48ace911cf504cfa3885a5ca2841db3429747
perugi <dominik.perusko@gmail.com>
2023-02-22 20:31:46 +0100

    Add solution for exercise 4|

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...values) {
+    for (value of values) {
+        index = array.indexOf(value);
+        // indexOf returns -1 if the value is not found in the array.
+        if (index >= 0) {
+            array.splice(index, 1);
+        }
+    }
 
+    return array;
 };
 
 // Do not edit below this line
