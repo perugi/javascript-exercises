d73adb277866d22e97c1de71407691db819c1819
perugi <dominik.perusko@gmail.com>
2023-03-02 22:55:19 +0100

    add solution for exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    return people.sort((a, b) => getAge(a) > getAge(b) ? -1 : 1)[0];
+};
 
+function getAge(person) {
+    if (!person.yearOfDeath) {
+        return new Date().getFullYear() - person.yearOfBirth;
+    } else {
+        return person.yearOfDeath - person.yearOfBirth;
+    };
 };
 
 // Do not edit below this line
