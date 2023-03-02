dd3416b40c45857bcd95cfd36e18016221a693e9
perugi <dominik.perusko@gmail.com>
2023-03-02 15:57:10 +0100

    Add solution for exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    chars = Array.from(string.toLowerCase());
+    chars = chars.filter(char => /[a-z]/.test(char));
 
+    for (i = 0; i < chars.length / 2; i++) {
+        if (chars[i] !== chars[(chars.length - 1) - i]) return false;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
