b261ce916ebc4bb1fbda3e4ae643cfa0d95532ab
perugi <dominik.perusko@gmail.com>
2023-03-02 15:38:28 +0100

    Add solution for exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  return numbers.reduce((sum, number) => sum += number, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  return numbers.reduce((sum, number) => sum *= number, 1);
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let fact = 1
+  for (; n > 1; n--) fact *= n;
+  return fact;
 };
 
 // Do not edit below this line
